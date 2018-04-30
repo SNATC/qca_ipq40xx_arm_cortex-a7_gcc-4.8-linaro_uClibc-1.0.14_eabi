3bf7c873d60c7fa47737b9b2ed99a41ad07c2568
OpenSNA <opensna@gmail.com>
2018-04-30 20:08:59 +0800

    first commit

---
+++ initial/lib/gcc/arm-openwrt-linux-uclibcgnueabi/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/build_dir/toolchain-arm_cortex-a7_gcc-4.8-linaro_uClibc-1.0.14_eabi/gcc-linaro-4.8-2014.04/configure --with-bugurl=https://dev.openwrt.org/ --with-pkgversion='OpenWrt/Linaro GCC 4.8-2014.04 r35193' --prefix=/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/staging_dir/toolchain-arm_cortex-a7_gcc-4.8-linaro_uClibc-1.0.14_eabi --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=arm-openwrt-linux-uclibcgnueabi --with-gnu-ld --enable-target-optspace --disable-libgomp --disable-libmudflap --disable-multilib --disable-nls --with-host-libstdcxx=-lstdc++ --with-float=soft --with-gmp=/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/staging_dir/host --with-mpfr=/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/staging_dir/host --with-mpc=/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/staging_dir/host --disable-decimal-float --disable-libssp --disable-__cxa_atexit --with-arch=armv7-a --with-newlib --with-sysroot=/opt/QNAP/ed5/ED5/qca-networking-2017-spf-5-2_qca_oem.git/qsdk/staging_dir/toolchain-arm_cortex-a7_gcc-4.8-linaro_uClibc-1.0.14_eabi --enable-languages=c --disable-shared --disable-threads";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv7-a" }, { "float", "soft" }, { "tls", "gnu" } };
